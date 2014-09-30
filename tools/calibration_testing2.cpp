f1e37654ad86bece3fe33c7b3d638f26f2a45fe9
Luca@adso <luca.penasa@gmail.com>
2014-09-30 17:19:59 +0200

    example with auodiff plus smoothness constrain
    Change-Id: I2828473dff69ab4d2ecbd1659666a3e5349202b2

--- tools/calibration_testing2.cpp
+++ tools/calibration_testing2.cpp
@@ -14,6 +14,38 @@ using Eigen::Matrix;
 
 //template class spc::RBFEvaluator<ceres::Jet<double, 6>>;
 
+class FixedModelPars
+{
+public:
+    double sigma_dist;
+    double sigma_angle;
+    Eigen::MatrixXd * knots_dist;
+    Eigen::MatrixXd * knots_angle;
+
+    const size_t getNumberOfDistanceKnots() const
+    {
+        return knots_dist->rows();
+    }
+
+    const size_t getNumberOfAngleKnots() const
+    {
+        return knots_angle->rows();
+    }
+};
+
+
+class Observation
+{
+public:
+    double intensity;
+    double intensity_std;
+    double distance;
+    double angle;
+    double eigen_ratio;
+    size_t cloud_id;
+    size_t core_id;
+
+};
 
 template <typename T>
 T compute_rbf(const Matrix<T, -1, 1> &coefficients,
@@ -28,7 +60,7 @@ T compute_rbf(const Matrix<T, -1, 1> &coefficients,
     Matrix<T, -1, -1> diff  = nodes.rowwise() - point.transpose();
     Matrix<T, -1, 1> sq_dist =  diff.rowwise().squaredNorm();
 
-   // transform these sq_dist in weights
+    // transform these sq_dist in weights
     Matrix<T, -1, 1> weights(sq_dist.rows());
 
     for (int i = 0; i < sq_dist.rows(); ++i)
@@ -38,78 +70,106 @@ T compute_rbf(const Matrix<T, -1, 1> &coefficients,
     return weights.cwiseProduct(coefficients ).sum();
 }
 
-template <size_t NDISTPARS, size_t NANGLEPARS>
-struct ResidualFunctor
+template <typename T>
+T predict_intensities(const Observation &ob, const FixedModelPars &fixed_pars,  T const * const * parameters)
 {
-    ResidualFunctor(double distance,
-                    double angle,
-                    double intensity,
-                    double sigma_dist,
-                    double sigma_angle,
-                    Eigen::MatrixXd * knots_dist,
-                    Eigen::MatrixXd * knots_angle):
-        distance_(distance),
-        angle_(angle),
-        intensity_(intensity),
-        sigma_dist_(sigma_dist),
-        sigma_angle_(sigma_angle)
+    typedef Eigen::Matrix<T,-1,1> MatrixType;
+    typedef Eigen::Map<const MatrixType> MapType;
 
+    const T * coeffs_dist = parameters[0];
+    const T * coeffs_angle = parameters[1];
+
+    MapType c_dist  (coeffs_dist, fixed_pars.getNumberOfDistanceKnots());
+    MapType c_angle  (coeffs_angle, fixed_pars.getNumberOfAngleKnots());
+
+
+    Eigen::Matrix<T, 1, 1> edist;
+    edist << T(ob.distance);
+
+    Eigen::Matrix<T, 1, 1> eang;
+    eang<< T(ob.angle);
+
+
+    T d_effect = compute_rbf<T>(c_dist, (fixed_pars.knots_dist)->template cast<T>(), T(fixed_pars.sigma_dist), edist );
+    T a_effect = compute_rbf<T>(c_angle, (fixed_pars.knots_angle)->template cast<T>(), T(fixed_pars.sigma_angle), eang );
+
+
+    T prediction = d_effect * a_effect;
+
+    return prediction;
+}
+
+struct SmoothnessConstrain
+{
+    SmoothnessConstrain(const double alpha, FixedModelPars const * fixed_pars ): alpha_(alpha)
     {
-        knots_dist_ = knots_dist;
-        knots_angle_ = knots_angle;
-//        std::cout  << "sigmas: " << distance << " " << angle << std::endl;
+        fixed_pars_ = fixed_pars;
     }
 
-    template <typename T> bool operator()(const T* const coeffs_dist, const T* const coeffs_angle, T* residual) const
+    template<typename T>
+    bool operator()(T const* const* parameters, T* residual) const
     {
 
-        Eigen::Matrix<T, NDISTPARS, 1> c_dist = Eigen::Map<const Eigen::Matrix<T, NDISTPARS, 1>>(coeffs_dist);
 
-        Eigen::Matrix<T, NANGLEPARS, 1> c_angle = Eigen::Map<const Eigen::Matrix<T, NANGLEPARS, 1>>(coeffs_angle);
-
-
-
-        Eigen::Matrix<T, 1, 1> edist;
-        edist << T(distance_);
-
-        Eigen::Matrix<T, 1, 1> eang;
-        eang<< T(angle_);
+        size_t d_s = fixed_pars_->getNumberOfDistanceKnots();
+        size_t a_s = fixed_pars_->getNumberOfAngleKnots();
 
 
-        T d_effect = compute_rbf<T>(c_dist, (*knots_dist_).template cast<T>(), T(sigma_dist_), edist );
-        T a_effect = compute_rbf<T>(c_angle, (*knots_angle_).template cast<T>(), T(sigma_angle_), eang );
 
 
-        T prediction = d_effect * a_effect;
-
-        double alpha = 0;
-
-        residual[0] = T(intensity_) - prediction;
-
-        for (int i = 0 ; i< NDISTPARS; ++i)
+        for (int i = 0; i < d_s; ++i)
         {
-            residual[i+1] = coeffs_dist[i] * alpha;
+            residual[i] = alpha_ * parameters[0][i];
         }
 
-        for (int i = 0;i< NANGLEPARS; ++i)
-
+        for (int i = 0; i < a_s; ++i)
         {
-            residual[i+1+NDISTPARS] = coeffs_angle[i] * alpha;
+            residual[i + d_s] = alpha_ * parameters[1][i];
         }
+
+
+
+
+
+
+        return true;
+    }
+
+    size_t getNumberOfResiduals() const
+    {
+        return fixed_pars_->getNumberOfAngleKnots() + fixed_pars_->getNumberOfDistanceKnots();
+    }
+
+    double alpha_;
+    const FixedModelPars * fixed_pars_;
+};
+
+struct ResidualFunctor
+{
+    ResidualFunctor(Observation * ob,
+                    FixedModelPars * fixed_pars)
+
+
+    {
+        observation_ = ob;
+        fixed_pars_ = fixed_pars;
+    }
+
+    template<typename T>
+    bool operator()(T const* const* parameters, T* residual) const
+    {
+
+        T prediction = predict_intensities(*observation_, *fixed_pars_, parameters);
+        residual[0] = T(observation_->intensity) - prediction;
+
 
         return true;
     }
 
 private:
-    const double distance_;
-    const double angle_;
-    const double intensity_;
-    const double sigma_dist_;
-    const double sigma_angle_;
+    Observation * observation_;
 
-
-    Eigen::MatrixXd * knots_dist_;
-    Eigen::MatrixXd * knots_angle_;
+    FixedModelPars * fixed_pars_;
 
 };
 
@@ -131,9 +191,34 @@ ObjT unique(const ObjT& b)
 }
 
 
+
+
+std::vector<Observation> table2observations(const spc::EigenTable & table)
+{
+    std::vector<Observation> out(table.getNumberOfRows()) ;
+
+    for (int i = 0; i < table.getNumberOfRows(); ++i)
+    {
+        Observation ob;
+
+        ob.intensity = table.row(i)(table.getColumnId("intensity"));
+        ob.intensity_std = table.row(i)(table.getColumnId("intensity_std"));
+        ob.distance = table.row(i)(table.getColumnId("distance"));
+        ob.angle = table.row(i)(table.getColumnId("angle"));
+        ob.eigen_ratio = table.row(i)(table.getColumnId("eigen_ratio"));
+        ob.cloud_id = table.row(i)(table.getColumnId("cloud_id"));
+        ob.core_id = table.row(i)(table.getColumnId("core_id"));
+
+        out.at(i) = ob;
+    }
+
+    return out;
+
+}
+
 int main (int argc, char ** argv)
 {
-    std::string datadb = "/home/luca/Desktop/calibration_db_small.spc";
+    std::string datadb = "/home/luca/Desktop/calibration_db.spc";
 
     ISerializable::Ptr o =  spc::io::deserializeFromFile(datadb);
 
@@ -147,11 +232,13 @@ int main (int argc, char ** argv)
         std::cout << table->getColumnName(i) << std::endl;
     }
 
-    Eigen::VectorXf i = table->mat().col(table->getColumnId("intensity"));
+    std::vector<Observation> obs = table2observations(*table);
+
     Eigen::VectorXf d = table->mat().col(table->getColumnId("distance"));
     Eigen::VectorXf a = table->mat().col(table->getColumnId("angle"));
+    Eigen::VectorXf i = table->mat().col(table->getColumnId("intensity"));
 
-    Eigen::VectorXf i_std = table->mat().col(table->getColumnId("intensity_std"));
+
 
     Eigen::VectorXi cloud_ids = table->mat().col(table->getColumnId("cloud_id")).cast<int>();
     Eigen::VectorXi c = unique(cloud_ids);
@@ -159,7 +246,7 @@ int main (int argc, char ** argv)
     std::cout << "unique cloud ids found: \n" << c << std::endl;
 
 
-//    std::cout << cloud_ids << std::endl;
+    //    std::cout << cloud_ids << std::endl;
     /////////////////////////////////////////////////////
 
     google::InitGoogleLogging(argv[0]);
@@ -176,7 +263,7 @@ int main (int argc, char ** argv)
 
     Eigen::MatrixXd * knots_angles =  new Eigen::MatrixXd(Eigen::VectorXd::LinSpaced (n_ang_pars, 0, 90));
 
-//    *knots_angles << 0.,  30, 60, 90;
+    //    *knots_angles << 0.,  30, 60, 90;
 
     std::cout << "node for distance " << std::endl;
     std::cout << *knots_distance << std::endl;
@@ -185,32 +272,61 @@ int main (int argc, char ** argv)
     std::cout << *knots_angles << std::endl;
 
 
-    double coeffs_dist[n_dist_pars];
-    for (int i = 0; i < n_dist_pars; ++i)
-        coeffs_dist[i] = 1;
-
     double dist_sigma = knots_distance->operator() (1) - knots_distance->operator ()(0);
-
-    double coeffs_angle[n_ang_pars];
-    for (int i = 0; i < n_ang_pars; ++i)
-        coeffs_angle[i] = 1;
-
     double angle_sigma = knots_angles->operator() (1) - knots_angles->operator ()(0);
 
 
+    FixedModelPars * fixed_pars = new FixedModelPars;
+    fixed_pars->knots_angle = knots_angles;
+    fixed_pars->knots_dist = knots_distance;
+    fixed_pars->sigma_dist = dist_sigma;
+    fixed_pars->sigma_angle = angle_sigma;
 
-    for (int j = 0 ; j < i.rows(); ++j)
+    std::vector<double *> parameters(2);
+    double dist_pars[n_dist_pars];
+    double angle_pars[n_ang_pars];
+
+    parameters[0] = &dist_pars[0];
+    parameters[1] = &angle_pars[0];
+
+    for (int i = 0; i < n_ang_pars; ++i)
+        angle_pars[i] = 1.0;
+
+    for (int i = 0; i < n_dist_pars; ++i)
+        dist_pars[i] = 1.0;
+
+    for (int j = 0 ; j < obs.size(); ++j)
     {
-        ResidualFunctor<n_dist_pars,n_ang_pars> * f = new ResidualFunctor<n_dist_pars,n_ang_pars>( d(j), a(j), i(j) , dist_sigma, angle_sigma, knots_distance, knots_angles);
 
-        problem.AddResidualBlock(new ceres::AutoDiffCostFunction< ResidualFunctor<n_dist_pars,n_ang_pars>, 1 + n_ang_pars + n_dist_pars, n_dist_pars, n_ang_pars> ( f ),
-                                 new ceres::ScaledLoss(NULL, 1/(i_std(j)*i_std(j)), ceres::TAKE_OWNERSHIP),
-//                                 NULL,
-//                                 HuberLoss(0.00001),
-                                 coeffs_dist,
-                                 coeffs_angle);
+        Observation* ob = &obs.at(j);
+        ResidualFunctor * f = new ResidualFunctor( ob, fixed_pars);
+
+        ceres::DynamicAutoDiffCostFunction<ResidualFunctor, 4> * cost  = new ceres::DynamicAutoDiffCostFunction< ResidualFunctor, 4> ( f );
+
+        cost->AddParameterBlock(n_dist_pars);
+        cost->AddParameterBlock(n_ang_pars);
+        cost->SetNumResiduals(1);
+
+
+
+
+        problem.AddResidualBlock(cost,
+                                 //                                 new ceres::ScaledLoss(NULL, 1/(i_std(j)*i_std(j)), ceres::TAKE_OWNERSHIP),
+                                 NULL,
+                                 //                                 HuberLoss(0.00001),
+                                 parameters
+                                 );
 
     }
+
+    SmoothnessConstrain * smoothness = new SmoothnessConstrain(0.05 * obs.size(), fixed_pars);
+    ceres::DynamicAutoDiffCostFunction<SmoothnessConstrain, 4> * cost2  = new ceres::DynamicAutoDiffCostFunction< SmoothnessConstrain, 4> ( smoothness );
+
+    cost2->AddParameterBlock(n_dist_pars);
+    cost2->AddParameterBlock(n_ang_pars);
+    cost2->SetNumResiduals(smoothness->getNumberOfResiduals());
+
+    problem.AddResidualBlock(cost2, NULL, parameters);
 
 
     ceres::Solver::Options options;
@@ -226,48 +342,31 @@ int main (int argc, char ** argv)
 
 
 
-    std::cout << "pars dist:" << std::endl;
-    for (int i =0; i <n_dist_pars; ++i)
+    std::cout << "dist pars:" << std::endl;
+    for (int i =0; i <n_dist_pars ; ++i)
     {
-        std::cout << coeffs_dist[i] << std::endl;
+        std::cout << dist_pars[i] << std::endl;
     }
 
-
-
-    std::cout << "pars angle:" << std::endl;
+    std::cout << "angle pars:" << std::endl;
     for (int i =0; i <n_ang_pars; ++i)
     {
-        std::cout << coeffs_angle[i] << std::endl;
+        std::cout << angle_pars[i] << std::endl;
     }
+
+
+
+
 
 
 
     /////////////////////////////////////////////// recomputing
 
-    Eigen::VectorXd eig_dist_coeffs = Eigen::Map<Matrix<double, n_dist_pars, 1>> (coeffs_dist);
+    Eigen::VectorXd predicted(obs.size());
 
-    Eigen::VectorXd eig_angle_coeffs = Eigen::Map<Matrix<double, n_ang_pars, 1>> (coeffs_angle);
-
-
-    Eigen::VectorXd predicted(i.rows());
-    Eigen::VectorXd dist_effect(i.rows());
-    Eigen::VectorXd angle_effect(i.rows());
-
-
-    for (int j = 0; j < i.rows(); ++j)
+    for (int j = 0; j < obs.size(); ++j)
     {
-        Eigen::Matrix<double, -1, 1> distance(1);
-        distance << d(j);
-
-        Eigen::Matrix<double, -1, 1> angle(1);
-        angle << a(j);
-
-        double valdist = compute_rbf<double>(eig_dist_coeffs, *knots_distance, dist_sigma, distance);
-        double valang = compute_rbf<double>(eig_angle_coeffs, *knots_angles, angle_sigma, angle);
-
-        predicted(j) = valdist * valang;
-        dist_effect(j) = valdist;
-        angle_effect(j) = valang;
+        predicted(j) = predict_intensities(obs.at(j), *fixed_pars,  &parameters[0]);
     }
 
     EigenTable::Ptr out (new EigenTable);
@@ -275,17 +374,15 @@ int main (int argc, char ** argv)
     out->addNewComponent("angle", 1);
     out->addNewComponent("intensity", 1);
     out->addNewComponent("pred_intensity", 1);
-    out->addNewComponent("angle_effect", 1);
-    out->addNewComponent("dist_effect", 1);
+
 
     out->resize(i.rows());
 
     out->column("distance") = d;
     out->column("angle") = a;
-    out->column("intensity") = i;
+    out->column("intensity") = i.cast<float>();
     out->column("pred_intensity") = predicted.cast<float>();
-    out->column("dist_effect") = dist_effect.cast<float>();
-    out->column("angle_effect") = angle_effect.cast<float>();
+
 
     spc::io::AsciiEigenTableWriter w;
     w.setInput(out);
