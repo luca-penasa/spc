b87bbc9aced1c4977073f91a5b3f0f87a5aa2b7e
Luca@adso <luca.penasa@gmail.com>
2015-03-18 14:21:21 +0100

    now gflags is optional. the library can compile without it
    Change-Id: Ib94667303fc8803ff9feb4e581d73b687c6a0f48

--- spc/core/macros_ptr.h
+++ spc/core/macros_ptr.h
@@ -2,20 +2,7 @@
 #ifndef MACROS_PTR_H
 #define MACROS_PTR_H
 
-#ifndef USE_STD_SHARED_PTR
-#include <boost/shared_ptr.hpp>
-#include <boost/make_shared.hpp>
 
-#define spcStaticPointerCast boost::static_pointer_cast
-#define spcDynamicPointerCast boost::dynamic_pointer_cast
-
-// a shared ptr
-#define spcSharedPtrMacro boost::shared_ptr
-
-// how to make one
-#define spcMakeSharedPtrMacro boost::make_shared
-
-#else // use std implementation
 #include <memory>
 
 #define spcStaticPointerCast std::static_pointer_cast
@@ -31,7 +18,6 @@
 // how to make one
 #define spcMakeSharedPtrMacro std::make_shared
 
-#endif
 
 #define spcTypedefSharedPtrs(classname)                                        \
     typedef spcSharedPtrMacro<classname> Ptr;                                  \
